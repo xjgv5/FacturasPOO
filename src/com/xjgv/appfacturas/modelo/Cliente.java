e02bc24daf6a1748d5d50fad772343f3db473c6a
Javier Gomez <javier.gov@outlook.es>
2023-10-20 20:52:42 -0600

    Creacion de clases

---
+++ src/com/xjgv/appfacturas/modelo/Cliente.java
@@ -0,0 +1,22 @@
+package com.xjgv.appfacturas.modelo;
+
+public class Cliente {
+    private  String nombre;
+    private String nif; //numero de identificacion fiscal
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+}
